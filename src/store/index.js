c253edf8903af069019da4f4bce9b9bb2bf3266b
HDLR <492587402@qq.com>
2019-04-29 13:11:34 +0800

    修改

--- src/store/index.js
+++ src/store/index.js
@@ -14,9 +14,9 @@ export default new Vuex.Store({
   mutations: {
     // 重置vuex本地储存状态
     resetStore (state) {
-      Object.keys(state).forEach((key) => {
-        state[key] = cloneDeep(window.SITE_CONFIG['storeState'][key])
-      })
+      // Object.keys(state).forEach((key) => {
+      //   state[key] = cloneDeep(window.SITE_CONFIG['storeState'][key])
+      // })
     }
   },
   strict: process.env.NODE_ENV !== 'production'
--- src/views/demo/ueditor.vue
+++ src/views/demo/ueditor.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="mod-demo-ueditor">
-    <el-alert
-      title="提示："
-      type="warning"
-      :closable="false">
-      <div slot-scope="description">
+    <el-alert title="提示" type="warning">
+      <div class="description">
         <p class="el-alert__description">文本编辑器ueditor因更新vue-cli为3.0版本后无法使用，后续再ueditor版本或者替换别的文本编辑器</p>
         <p class="el-alert__description">此Demo只提供UEditor官方使用文档，入门部署和体验功能。具体使用请参考：http://fex.baidu.com/ueditor/</p>
       </div>
